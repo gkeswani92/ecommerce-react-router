720b634759548bf5ddb4910a12d4be08c275488a
gkeswani92 <gauravkeswani92@gmail.com>
2018-09-08 19:21:59 -0700

    Converting the anchor tags with react-router Links to render the right component using Switch

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = (props) => (
   <div id="myNav" className="overlay">
     <a className="closebtn" onClick={() => props.closeNav()}>&times;</a>
     <div className="overlay-content">
-      <a href="">Home</a>  
-      <a href="">Vitamin Juice</a>
+      {/* 
+        The path defined in the 'to' section of the Link component is what the switch 
+        will be looking for in the routes.
+      */}
+      <Link to="/" onClick={() => props.closeNav()}>Home</Link>
+      <Link to="/vitamin" onClick={() => props.closeNav()}>Vitamin Juice</Link>
       <a href="">Clothing</a>
       <a href="">Supplements</a>
       <a href="">Contact</a>
