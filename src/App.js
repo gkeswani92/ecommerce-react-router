64f9912ccda956d9ba76bee2e63dec94016da852
gkeswani92 <gauravkeswani92@gmail.com>
2018-09-08 19:18:06 -0700

    Using Router, Switch and Route to setup routes that render their respective components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import logo from './logo.png';
 import './App.css';
 import Home from './components/Home';
 import Navigation from './components/Navigation';
+import Vitamin from './components/Vitamin';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      toggleLogo: true,
+      toggleLogo:  true,
     }
     this.toggleLogo = this.toggleLogo.bind(this);
     this.openNav = this.openNav.bind(this);
@@ -31,23 +34,45 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} 
-            className={this.state.toggleLogo ? 'static-logo' : 'static-logo animated jello'} 
-            alt="logo"
-            onMouseEnter={this.toggleLogo}
-            onMouseLeave={this.toggleLogo}
-            onClick={this.openNav}
-          />
-          <h1
-            className={this.state.toggleLogo ? 'menu-hidden' : 'menu animated bounceInDown'}
-            onClick={this.openNav}
-          >Menu</h1>
-          <Navigation closeNav={this.closeNav} />
-        </header>
-        <Home />
-      </div>
+      <Router>
+        {/* Router is the component that has to wrap the entire application */}
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} 
+              className={this.state.toggleLogo ? 'static-logo' : 'static-logo animated jello'} 
+              alt="logo"
+              onMouseEnter={this.toggleLogo}
+              onMouseLeave={this.toggleLogo}
+              onClick={this.openNav}
+            />
+            <h1
+              className={this.state.toggleLogo ? 'menu-hidden' : 'menu animated bounceInDown'}
+              onClick={this.openNav}
+            >Menu</h1>
+            <Navigation closeNav={this.closeNav} />
+          </header>
+          {/*  
+            - The switch component allows us to switch between routes.
+            - It goes through all routes until one matches. The one that matches
+            then renders its corresponding component.
+            - If it does not match with any route, we can set a default route 
+            that will be executed.
+          */}
+          <Switch>
+            {/* 
+            This path will only be called if someone is coming to the '/' path of the website
+            since we are using exact path
+             */}
+            <Route exact path="/" component={Home} />
+
+            {/*
+            This path will be called as far as someone uses a path that starts with the vitamin
+            i.e. /vitamin or /vitamin/productA etc.
+            */}
+            <Route path="/vitamin" component={Vitamin} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Vitamin.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Vitamin = (props) => {
+    return <h1>Vitamin Page</h1>;
+}
+ 
+export default Vitamin;
